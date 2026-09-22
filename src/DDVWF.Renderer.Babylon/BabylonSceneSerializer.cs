502e27f1a8a9748ebefd2e3aa5b8d3320bc808c1
Wwarff <111942146+Wwarff@users.noreply.github.com>
2026-09-21 23:03:38 -0400

    Include all referenced Sage animated texture frames

--- src/DDVWF.Renderer.Babylon/BabylonSceneSerializer.cs
+++ src/DDVWF.Renderer.Babylon/BabylonSceneSerializer.cs
@@ -6,7 +6,7 @@ public static class BabylonSceneSerializer
  {
   var meshes=zone.Entities.Where(e=>e.ClientAsset is not null&&assets.ContainsKey(e.ClientAsset)).Select(e=>new{e.Id,e.Name,Kind=e.Kind.ToString(),e.Position,e.Scale,Asset=assets[e.ClientAsset!]}).ToArray();
   var markers=zone.Entities.Where(e=>e.ServerId is not null&&(e.ClientAsset is null||!assets.ContainsKey(e.ClientAsset))).Select(e=>new{e.Id,e.Name,Kind=e.Kind.ToString(),e.Position,e.Scale,e.ServerId}).ToArray();
-  var textureData=(textures??new Dictionary<string,byte[]>()).Where(x=>meshes.Any(m=>m.Asset.Primitives.Any(p=>string.Equals(p.TextureName,x.Key,StringComparison.OrdinalIgnoreCase)))).ToDictionary(x=>x.Key,x=>Convert.ToBase64String(x.Value),StringComparer.OrdinalIgnoreCase);
+  var textureData=(textures??new Dictionary<string,byte[]>()).Where(x=>meshes.Any(m=>m.Asset.Primitives.Any(p=>string.Equals(p.TextureName,x.Key,StringComparison.OrdinalIgnoreCase)||(p.Material?.TextureFrames?.Any(t=>string.Equals(t,x.Key,StringComparison.OrdinalIgnoreCase))??false)))).ToDictionary(x=>x.Key,x=>Convert.ToBase64String(x.Value),StringComparer.OrdinalIgnoreCase);
   return JsonSerializer.Serialize(new{zone=zone.ShortName,meshes,markers,textures=textureData},new JsonSerializerOptions{PropertyNamingPolicy=JsonNamingPolicy.CamelCase});
  }
 }
