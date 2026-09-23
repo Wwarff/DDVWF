31b47e77674d6871cccb25bd854b92b6b3c75f11
Wwarff <111942146+Wwarff@users.noreply.github.com>
2026-09-22 23:25:06 -0400

    Join EQEmu spawn candidate filters into NPC entities

--- src/DDVWF.Core/Zone/ServerZoneJoiner.cs
+++ src/DDVWF.Core/Zone/ServerZoneJoiner.cs
@@ -31,7 +31,7 @@ public static class ServerZoneJoiner
                     npc.Size <= 0 ? 1f : npc.Size,
                     spawn.Id,
                     null,
-                    new NpcEntityData(npc.Id,spawn.SpawnGroupId,entry.Chance,npc.Race,npc.Gender,npc.Model,npc.Texture,npc.HelmTexture,npc.HeroForgeModel,npc.Face,npc.HairStyle,npc.HairColor,npc.EyeColor1,npc.EyeColor2,npc.BeardColor,npc.Beard,npc.DrakkinHeritage,npc.DrakkinTattoo,npc.DrakkinDetails,npc.ArmTexture,npc.BracerTexture,npc.HandTexture,npc.LegTexture,npc.FeetTexture,npc.PrimaryWeaponTexture,npc.SecondaryWeaponTexture,npc.Light)));
+                    new NpcEntityData(npc.Id,spawn.SpawnGroupId,entry.Chance,npc.Race,npc.Gender,npc.Model,npc.Texture,npc.HelmTexture,npc.HeroForgeModel,npc.Face,npc.HairStyle,npc.HairColor,npc.EyeColor1,npc.EyeColor2,npc.BeardColor,npc.Beard,npc.DrakkinHeritage,npc.DrakkinTattoo,npc.DrakkinDetails,npc.ArmTexture,npc.BracerTexture,npc.HandTexture,npc.LegTexture,npc.FeetTexture,npc.PrimaryWeaponTexture,npc.SecondaryWeaponTexture,npc.Light,entry.ConditionValueFilter,entry.MinTime,entry.MaxTime,entry.MinExpansion,entry.MaxExpansion,entry.ContentFlags,entry.ContentFlagsDisabled)));
             }
         }
 
