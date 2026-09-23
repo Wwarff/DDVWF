2527111495db04b35b23fcc70fa105daa3ebc1bd
Wwarff <111942146+Wwarff@users.noreply.github.com>
2026-09-22 23:21:41 -0400

    Read EQEmu NPC appearance and model fields

--- src/DDVWF.Server/EqEmuReadProvider.cs
+++ src/DDVWF.Server/EqEmuReadProvider.cs
@@ -40,10 +40,10 @@ public sealed class EqEmuReadProvider : IServerDataProvider
         foreach(var id in entries.Select(x=>x.NpcId).Distinct())
         {
             await using var cmd=connection.CreateCommand();
-            cmd.CommandText="SELECT id, name, race, gender, size FROM npc_types WHERE id = @id";
+            cmd.CommandText="SELECT id,name,race,gender,size,model,texture,helmtexture,herosforgemodel,face,luclin_hairstyle,luclin_haircolor,luclin_eyecolor,luclin_eyecolor2,luclin_beardcolor,luclin_beard,drakkin_heritage,drakkin_tattoo,drakkin_details,armtexture,bracertexture,handtexture,legtexture,feettexture,d_melee_texture1,d_melee_texture2,light FROM npc_types WHERE id = @id";
             Add(cmd,"@id",id);
             await using var r=await cmd.ExecuteReaderAsync(cancellationToken);
-            if(await r.ReadAsync(cancellationToken)) npcs[id]=new(r.GetInt64(0),r.GetString(1),r.GetInt32(2),r.GetInt32(3),F(r,4));
+            if(await r.ReadAsync(cancellationToken)) npcs[id]=new(r.GetInt64(0),r.GetString(1),r.GetInt32(2),r.GetInt32(3),F(r,4),I(r,5),I(r,6),I(r,7),I(r,8),I(r,9),I(r,10),I(r,11),I(r,12),I(r,13),I(r,14),I(r,15),I(r,16),I(r,17),I(r,18),I(r,19),I(r,20),I(r,21),I(r,22),I(r,23),I(r,24),I(r,25),I(r,26));
         }
 
         await using(var cmd=connection.CreateCommand())
@@ -67,6 +67,6 @@ public sealed class EqEmuReadProvider : IServerDataProvider
         ServerZoneJoiner.Join(zone,new ServerZoneSnapshot(spawn2,entries,npcs.Values.ToArray(),doors,zonePoints));
     }
 
-    private static float F(DbDataReader r,int i)=>Convert.ToSingle(r.GetValue(i),System.Globalization.CultureInfo.InvariantCulture);
+    private static float F(DbDataReader r,int i)=>Convert.ToSingle(r.GetValue(i),System.Globalization.CultureInfo.InvariantCulture);\n    private static int I(DbDataReader r,int i)=>r.IsDBNull(i)?0:Convert.ToInt32(r.GetValue(i),System.Globalization.CultureInfo.InvariantCulture);
     private static void Add(DbCommand command,string name,object value){var p=command.CreateParameter();p.ParameterName=name;p.Value=value;command.Parameters.Add(p);}
 }
