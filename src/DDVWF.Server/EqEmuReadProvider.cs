1e6181534fe81a6a2a098a1501a56e3d448990a7
Wwarff <111942146+Wwarff@users.noreply.github.com>
2026-09-23 15:24:42 -0400

    Match EQEmu active zone-version content filtering

--- src/DDVWF.Server/EqEmuReadProvider.cs
+++ src/DDVWF.Server/EqEmuReadProvider.cs
@@ -7,11 +7,11 @@ public sealed record EqEmuConnectionOptions(string Host, int Port, string Databa
 
 public sealed class EqEmuReadProvider : IServerDataProvider, IServerReadDiagnosticsProvider
 {
-    private readonly Func<CancellationToken,Task<DbConnection>> _open;
+    private readonly Func<CancellationToken,Task<DbConnection>> _open;private readonly int _zoneVersion;
     public bool CanWrite => false;
     public ServerReadDiagnostics? LastReadDiagnostics { get; private set; }
 
-    public EqEmuReadProvider(Func<CancellationToken,Task<DbConnection>> open) => _open=open;
+    public EqEmuReadProvider(Func<CancellationToken,Task<DbConnection>> open,int zoneVersion=0) => (_open,_zoneVersion)=(open,zoneVersion);
 
     public async Task PopulateAsync(CompleteZone zone,CancellationToken cancellationToken)
     {
@@ -21,8 +21,8 @@ public sealed class EqEmuReadProvider : IServerDataProvider, IServerReadDiagnost
 
         await using(var cmd=connection.CreateCommand())
         {
-            cmd.CommandText="SELECT id,spawngroupID,zone,x,y,z,heading,respawntime,variance,pathgrid,version,path_when_zone_idle,_condition,cond_value,animation,min_expansion,max_expansion,content_flags,content_flags_disabled FROM spawn2 WHERE zone = @zone";
-            Add(cmd,"@zone",zone.ShortName);
+            cmd.CommandText="SELECT id,spawngroupID,zone,x,y,z,heading,respawntime,variance,pathgrid,version,path_when_zone_idle,_condition,cond_value,animation,min_expansion,max_expansion,content_flags,content_flags_disabled FROM spawn2 WHERE zone = @zone AND (version = @version OR version = -1)";
+            Add(cmd,"@zone",zone.ShortName);Add(cmd,"@version",_zoneVersion);
             await using var r=await cmd.ExecuteReaderAsync(cancellationToken);
             while(await r.ReadAsync(cancellationToken))
                 spawn2.Add(new(r.GetInt64(0),r.GetInt64(1),r.GetString(2),F(r,3),F(r,4),F(r,5),F(r,6),I(r,7),I(r,8),I(r,9),I(r,10),I(r,11)!=0,I(r,12),I(r,13),I(r,14),I(r,15),I(r,16),r.IsDBNull(17)?"":r.GetString(17),r.IsDBNull(18)?"":r.GetString(18)));
@@ -69,20 +69,20 @@ public sealed class EqEmuReadProvider : IServerDataProvider, IServerReadDiagnost
         await using(var cmd=connection.CreateCommand())
         {
             cmd.CommandText="SELECT id,zoneid,type,type2 FROM grid WHERE zoneid=@zoneid";
-            Add(cmd,"@zoneid",zoneId);await using var r=await cmd.ExecuteReaderAsync(cancellationToken);
+            Add(cmd,"@zoneid",zoneId);Add(cmd,"@version",_zoneVersion);await using var r=await cmd.ExecuteReaderAsync(cancellationToken);
             while(await r.ReadAsync(cancellationToken))grids.Add(new(I(r,0),Convert.ToUInt32(r.GetValue(1),System.Globalization.CultureInfo.InvariantCulture),I(r,2),I(r,3)));
         }
         await using(var cmd=connection.CreateCommand())
         {
             cmd.CommandText="SELECT gridid,zoneid,number,x,y,z,heading,pause,centerpoint FROM grid_entries WHERE zoneid=@zoneid";
-            Add(cmd,"@zoneid",zoneId);await using var r=await cmd.ExecuteReaderAsync(cancellationToken);
+            Add(cmd,"@zoneid",zoneId);Add(cmd,"@version",_zoneVersion);await using var r=await cmd.ExecuteReaderAsync(cancellationToken);
             while(await r.ReadAsync(cancellationToken))gridEntries.Add(new(I(r,0),Convert.ToUInt32(r.GetValue(1),System.Globalization.CultureInfo.InvariantCulture),I(r,2),F(r,3),F(r,4),F(r,5),F(r,6),I(r,7),I(r,8)!=0));
         }
 
         await using(var cmd=connection.CreateCommand())
         {
-            cmd.CommandText="SELECT id,zoneid,version,max_x,max_y,max_z,min_x,min_y,heading,name,item,max_allowed,comment,respawn_timer,fix_z,min_expansion,max_expansion,content_flags,content_flags_disabled FROM ground_spawns WHERE zoneid = @zoneid";
-            Add(cmd,"@zoneid",zoneId);
+            cmd.CommandText="SELECT id,zoneid,version,max_x,max_y,max_z,min_x,min_y,heading,name,item,max_allowed,comment,respawn_timer,fix_z,min_expansion,max_expansion,content_flags,content_flags_disabled FROM ground_spawns WHERE zoneid = @zoneid AND (version = @version OR version = -1)";
+            Add(cmd,"@zoneid",zoneId);Add(cmd,"@version",_zoneVersion);
             await using var r=await cmd.ExecuteReaderAsync(cancellationToken);
             while(await r.ReadAsync(cancellationToken))
                 groundSpawns.Add(new(r.GetInt64(0),Convert.ToUInt32(r.GetValue(1),System.Globalization.CultureInfo.InvariantCulture),I(r,2),F(r,3),F(r,4),F(r,5),F(r,6),F(r,7),F(r,8),r.IsDBNull(9)?"":r.GetString(9),I(r,10),I(r,11),r.IsDBNull(12)?"":r.GetString(12),I(r,13),I(r,14)!=0,I(r,15),I(r,16),r.IsDBNull(17)?"":r.GetString(17),r.IsDBNull(18)?"":r.GetString(18)));
@@ -90,8 +90,8 @@ public sealed class EqEmuReadProvider : IServerDataProvider, IServerReadDiagnost
 
         await using(var cmd=connection.CreateCommand())
         {
-            cmd.CommandText="SELECT id,zoneid,version,xpos,ypos,zpos,heading,itemid,charges,objectname,type,icon,size_percentage,unknown24,unknown60,unknown64,unknown68,unknown72,unknown76,unknown84,size,solid_type,incline,tilt_x,tilt_y,display_name,min_expansion,max_expansion,content_flags,content_flags_disabled FROM object WHERE zoneid = @zoneid";
-            Add(cmd,"@zoneid",zoneId);
+            cmd.CommandText="SELECT id,zoneid,version,xpos,ypos,zpos,heading,itemid,charges,objectname,type,icon,size_percentage,unknown24,unknown60,unknown64,unknown68,unknown72,unknown76,unknown84,size,solid_type,incline,tilt_x,tilt_y,display_name,min_expansion,max_expansion,content_flags,content_flags_disabled FROM object WHERE zoneid = @zoneid AND (version = @version OR version = -1)";
+            Add(cmd,"@zoneid",zoneId);Add(cmd,"@version",_zoneVersion);
             await using var r=await cmd.ExecuteReaderAsync(cancellationToken);
             while(await r.ReadAsync(cancellationToken))
                 objects.Add(new(r.GetInt64(0),Convert.ToUInt32(r.GetValue(1),System.Globalization.CultureInfo.InvariantCulture),I(r,2),F(r,3),F(r,4),F(r,5),F(r,6),I(r,7),I(r,8),r.IsDBNull(9)?"":r.GetString(9),I(r,10),I(r,11),F(r,12),I(r,13),I(r,14),I(r,15),I(r,16),I(r,17),I(r,18),I(r,19),F(r,20),I(r,21),I(r,22),F(r,23),F(r,24),r.IsDBNull(25)?"":r.GetString(25),I(r,26),I(r,27),r.IsDBNull(28)?"":r.GetString(28),r.IsDBNull(29)?"":r.GetString(29)));
@@ -100,7 +100,7 @@ public sealed class EqEmuReadProvider : IServerDataProvider, IServerReadDiagnost
         await using(var cmd=connection.CreateCommand())
         {
             cmd.CommandText="SELECT zoneid,parentid,bagidx,itemid,charges,droptime,augslot1,augslot2,augslot3,augslot4,augslot5,augslot6 FROM object_contents WHERE zoneid = @zoneid";
-            Add(cmd,"@zoneid",zoneId);
+            Add(cmd,"@zoneid",zoneId);Add(cmd,"@version",_zoneVersion);
             await using var r=await cmd.ExecuteReaderAsync(cancellationToken);
             while(await r.ReadAsync(cancellationToken))
                 objectContents.Add(new(Convert.ToUInt32(r.GetValue(0),System.Globalization.CultureInfo.InvariantCulture),Convert.ToInt64(r.GetValue(1),System.Globalization.CultureInfo.InvariantCulture),I(r,2),I(r,3),I(r,4),r.IsDBNull(5)?null:Convert.ToDateTime(r.GetValue(5),System.Globalization.CultureInfo.InvariantCulture),I(r,6),I(r,7),I(r,8),I(r,9),I(r,10),I(r,11)));
@@ -108,8 +108,8 @@ public sealed class EqEmuReadProvider : IServerDataProvider, IServerReadDiagnost
 
         await using(var cmd=connection.CreateCommand())
         {
-            cmd.CommandText="SELECT id,doorid,zone,name,pos_x,pos_y,pos_z,heading,opentype,size,version,lockpick,keyitem,triggerdoor,triggertype,doorisopen,dest_zone,dest_instance,dest_x,dest_y,dest_z,dest_heading,invert_state,incline FROM doors WHERE zone = @zone";
-            Add(cmd,"@zone",zone.ShortName);
+            cmd.CommandText="SELECT id,doorid,zone,name,pos_x,pos_y,pos_z,heading,opentype,size,version,lockpick,keyitem,triggerdoor,triggertype,doorisopen,dest_zone,dest_instance,dest_x,dest_y,dest_z,dest_heading,invert_state,incline FROM doors WHERE zone = @zone AND (version = @version OR version = -1)";
+            Add(cmd,"@zone",zone.ShortName);Add(cmd,"@version",_zoneVersion);
             await using var r=await cmd.ExecuteReaderAsync(cancellationToken);
             while(await r.ReadAsync(cancellationToken))
                 doors.Add(new(r.GetInt64(0),I(r,1),r.GetString(2),r.GetString(3),F(r,4),F(r,5),F(r,6),F(r,7),I(r,8),I(r,9),I(r,10),I(r,11),I(r,12),I(r,13),I(r,14),I(r,15)!=0,r.IsDBNull(16)?"NONE":r.GetString(16),r.IsDBNull(17)?0:Convert.ToUInt32(r.GetValue(17),System.Globalization.CultureInfo.InvariantCulture),F(r,18),F(r,19),F(r,20),F(r,21),I(r,22),I(r,23)));
@@ -117,8 +117,8 @@ public sealed class EqEmuReadProvider : IServerDataProvider, IServerReadDiagnost
 
         await using(var cmd=connection.CreateCommand())
         {
-            cmd.CommandText="SELECT id,zone,version,number,x,y,z,heading,target_x,target_y,target_z,target_heading,target_zone_id,target_instance,zoneinst,buffer,client_version_mask,min_expansion,max_expansion,content_flags,content_flags_disabled,is_virtual,height,width FROM zone_points WHERE zone = @zone";
-            Add(cmd,"@zone",zone.ShortName);
+            cmd.CommandText="SELECT id,zone,version,number,x,y,z,heading,target_x,target_y,target_z,target_heading,target_zone_id,target_instance,zoneinst,buffer,client_version_mask,min_expansion,max_expansion,content_flags,content_flags_disabled,is_virtual,height,width FROM zone_points WHERE zone = @zone AND (version = @version OR version = -1)";
+            Add(cmd,"@zone",zone.ShortName);Add(cmd,"@version",_zoneVersion);
             await using var r=await cmd.ExecuteReaderAsync(cancellationToken);
             while(await r.ReadAsync(cancellationToken))
                 zonePoints.Add(new(r.GetInt64(0),r.GetString(1),I(r,2),I(r,3),F(r,4),F(r,5),F(r,6),F(r,7),F(r,8),F(r,9),F(r,10),F(r,11),Convert.ToUInt32(r.GetValue(12),System.Globalization.CultureInfo.InvariantCulture),Convert.ToUInt32(r.GetValue(13),System.Globalization.CultureInfo.InvariantCulture),I(r,14),F(r,15),r.IsDBNull(16)?0xFFFFFFFF:Convert.ToUInt32(r.GetValue(16),System.Globalization.CultureInfo.InvariantCulture),I(r,17),I(r,18),r.IsDBNull(19)?"":r.GetString(19),r.IsDBNull(20)?"":r.GetString(20),I(r,21)!=0,I(r,22),I(r,23)));
