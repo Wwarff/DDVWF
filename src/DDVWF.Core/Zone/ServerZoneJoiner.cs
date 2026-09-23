8bda70af977892ae55da1fcb97d2c249e9b94012
Wwarff <111942146+Wwarff@users.noreply.github.com>
2026-09-23 13:51:49 -0400

    Join EQEmu object contents into Complete Zone objects

--- src/DDVWF.Core/Zone/ServerZoneJoiner.cs
+++ src/DDVWF.Core/Zone/ServerZoneJoiner.cs
@@ -86,7 +86,7 @@ public static class ServerZoneJoiner
                 obj.Size>0?obj.Size/100f:1f,
                 obj.Id,
                 native,
-                new ObjectEntityData(obj.Version,obj.ItemId,obj.Charges,obj.Type,obj.Icon,obj.SizePercentage,obj.Unknown24,obj.Unknown60,obj.Unknown64,obj.Unknown68,obj.Unknown72,obj.Unknown76,obj.Unknown84,obj.Size,obj.SolidType,obj.Incline,obj.TiltX,obj.TiltY,obj.DisplayName,obj.MinExpansion,obj.MaxExpansion,obj.ContentFlags,obj.ContentFlagsDisabled),
+                new ObjectEntityData(obj.Version,obj.ItemId,obj.Charges,obj.Type,obj.Icon,obj.SizePercentage,obj.Unknown24,obj.Unknown60,obj.Unknown64,obj.Unknown68,obj.Unknown72,obj.Unknown76,obj.Unknown84,obj.Size,obj.SolidType,obj.Incline,obj.TiltX,obj.TiltY,obj.DisplayName,obj.MinExpansion,obj.MaxExpansion,obj.ContentFlags,obj.ContentFlagsDisabled,(snapshot.ObjectContents??Array.Empty<ObjectContentRecord>()).Where(x=>x.ParentId==obj.Id).Select(x=>new ObjectContentData(x.BagIndex,x.ItemId,x.Charges,x.DropTime,x.AugSlot1,x.AugSlot2,x.AugSlot3,x.AugSlot4,x.AugSlot5,x.AugSlot6)).ToArray()),
                 new EqRotation(obj.TiltX,obj.Heading,obj.TiltY)));
         }
 
