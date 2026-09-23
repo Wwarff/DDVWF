03550483904580bec3312ee17588c3b58e08e23b
Wwarff <111942146+Wwarff@users.noreply.github.com>
2026-09-23 11:55:14 -0400

    Read live EQEmu world objects for active zone

--- src/DDVWF.Server/EqEmuReadProvider.cs
+++ src/DDVWF.Server/EqEmuReadProvider.cs
@@ -16,7 +16,7 @@ public sealed class EqEmuReadProvider : IServerDataProvider
     {
         await using var connection=await _open(cancellationToken);
         var spawn2=new List<Spawn2Record>(); var entries=new List<SpawnEntryRecord>();
-        var npcs=new Dictionary<long,NpcTypeRecord>(); var doors=new List<DoorRecord>(); var zonePoints=new List<ZonePointRecord>(); var spawnGroups=new List<SpawnGroupRecord>();
+        var npcs=new Dictionary<long,NpcTypeRecord>(); var doors=new List<DoorRecord>(); var zonePoints=new List<ZonePointRecord>(); var spawnGroups=new List<SpawnGroupRecord>(); var objects=new List<ObjectRecord>();
 
         await using(var cmd=connection.CreateCommand())
         {
@@ -55,6 +55,25 @@ public sealed class EqEmuReadProvider : IServerDataProvider
             if(await r.ReadAsync(cancellationToken)) npcs[id]=new(r.GetInt64(0),r.GetString(1),r.GetInt32(2),r.GetInt32(3),F(r,4),I(r,5),I(r,6),I(r,7),I(r,8),I(r,9),I(r,10),I(r,11),I(r,12),I(r,13),I(r,14),I(r,15),I(r,16),I(r,17),I(r,18),I(r,19),I(r,20),I(r,21),I(r,22),I(r,23),I(r,24),I(r,25),I(r,26));
         }
 
+        uint zoneId;
+        await using(var zoneCmd=connection.CreateCommand())
+        {
+            zoneCmd.CommandText="SELECT zoneidnumber FROM zone WHERE short_name = @zone ORDER BY version LIMIT 1";
+            Add(zoneCmd,"@zone",zone.ShortName);
+            var value=await zoneCmd.ExecuteScalarAsync(cancellationToken);
+            if(value is null||value is DBNull)throw new InvalidDataException($"EQEmu zone row was not found for '{zone.ShortName}'.");
+            zoneId=Convert.ToUInt32(value,System.Globalization.CultureInfo.InvariantCulture);
+        }
+
+        await using(var cmd=connection.CreateCommand())
+        {
+            cmd.CommandText="SELECT id,zoneid,version,xpos,ypos,zpos,heading,itemid,charges,objectname,type,icon,size_percentage,unknown24,unknown60,unknown64,unknown68,unknown72,unknown76,unknown84,size,solid_type,incline,tilt_x,tilt_y,display_name,min_expansion,max_expansion,content_flags,content_flags_disabled FROM object WHERE zoneid = @zoneid";
+            Add(cmd,"@zoneid",zoneId);
+            await using var r=await cmd.ExecuteReaderAsync(cancellationToken);
+            while(await r.ReadAsync(cancellationToken))
+                objects.Add(new(r.GetInt64(0),Convert.ToUInt32(r.GetValue(1),System.Globalization.CultureInfo.InvariantCulture),I(r,2),F(r,3),F(r,4),F(r,5),F(r,6),I(r,7),I(r,8),r.IsDBNull(9)?"":r.GetString(9),I(r,10),I(r,11),F(r,12),I(r,13),I(r,14),I(r,15),I(r,16),I(r,17),I(r,18),I(r,19),F(r,20),I(r,21),I(r,22),F(r,23),F(r,24),r.IsDBNull(25)?"":r.GetString(25),I(r,26),I(r,27),r.IsDBNull(28)?"":r.GetString(28),r.IsDBNull(29)?"":r.GetString(29)));
+        }
+
         await using(var cmd=connection.CreateCommand())
         {
             cmd.CommandText="SELECT id,doorid,zone,name,pos_x,pos_y,pos_z,heading,opentype,size,version,lockpick,keyitem,triggerdoor,triggertype,doorisopen,dest_zone,dest_instance,dest_x,dest_y,dest_z,dest_heading,invert_state,incline FROM doors WHERE zone = @zone";
@@ -73,7 +92,7 @@ public sealed class EqEmuReadProvider : IServerDataProvider
                 zonePoints.Add(new(r.GetInt64(0),r.GetString(1),I(r,2),I(r,3),F(r,4),F(r,5),F(r,6),F(r,7),F(r,8),F(r,9),F(r,10),F(r,11),Convert.ToUInt32(r.GetValue(12),System.Globalization.CultureInfo.InvariantCulture),Convert.ToUInt32(r.GetValue(13),System.Globalization.CultureInfo.InvariantCulture),I(r,14),F(r,15),r.IsDBNull(16)?0xFFFFFFFF:Convert.ToUInt32(r.GetValue(16),System.Globalization.CultureInfo.InvariantCulture),I(r,17),I(r,18),r.IsDBNull(19)?"":r.GetString(19),r.IsDBNull(20)?"":r.GetString(20),I(r,21)!=0,I(r,22),I(r,23)));
         }
 
-        ServerZoneJoiner.Join(zone,new ServerZoneSnapshot(spawn2,entries,npcs.Values.ToArray(),doors,zonePoints,spawnGroups));
+        ServerZoneJoiner.Join(zone,new ServerZoneSnapshot(spawn2,entries,npcs.Values.ToArray(),doors,zonePoints,spawnGroups,objects));
     }
 
     private static float F(DbDataReader r,int i)=>Convert.ToSingle(r.GetValue(i),System.Globalization.CultureInfo.InvariantCulture);
