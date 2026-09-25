b3aab7d3aa6a29bc74c407c3dd2ccb459f46ec19
Wwarff <111942146+Wwarff@users.noreply.github.com>
2026-09-25 18:46:00 -0400

    Match Sage SQL door scale exactly

--- src/DDVWF.Core/Zone/ServerZoneJoiner.cs
+++ src/DDVWF.Core/Zone/ServerZoneJoiner.cs
@@ -98,7 +98,7 @@ public static class ServerZoneJoiner
                 ZoneEntityKind.Door,
                 door.Model,
                 new EqPosition(door.X, door.Y, door.Z, door.Heading),
-                door.Size <= 0 ? 1f : door.Size / 100f,
+                door.Size / 100f,
                 door.Id,
                 door.Model,
                 new DoorEntityData(door.DoorId,door.OpenType,door.Version,door.Lockpick,door.KeyItem,door.TriggerDoor,door.TriggerType,door.DoorIsOpen,door.DestZone,door.DestInstance,door.DestX,door.DestY,door.DestZ,door.DestHeading,door.InvertState,door.Incline)));
