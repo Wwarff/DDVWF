0b036540376c62c5c2c40e92ad50a01ab0fd8ba4
Wwarff <111942146+Wwarff@users.noreply.github.com>
2026-09-26 12:04:04 -0400

    Match EQEmu ground spawn definition cap

--- src/DDVWF.Server/EqEmuReadProvider.cs
+++ src/DDVWF.Server/EqEmuReadProvider.cs
@@ -82,7 +82,7 @@ public sealed class EqEmuReadProvider : IServerDataProvider, IServerReadDiagnost
 
         await using(var cmd=connection.CreateCommand())
         {
-            cmd.CommandText="SELECT id,zoneid,version,max_x,max_y,max_z,min_x,min_y,heading,name,item,max_allowed,comment,respawn_timer,fix_z,min_expansion,max_expansion,content_flags,content_flags_disabled FROM ground_spawns WHERE zoneid = @zoneid AND (version = @version OR version = -1)";
+            cmd.CommandText="SELECT id,zoneid,version,max_x,max_y,max_z,min_x,min_y,heading,name,item,max_allowed,comment,respawn_timer,fix_z,min_expansion,max_expansion,content_flags,content_flags_disabled FROM ground_spawns WHERE zoneid = @zoneid AND (version = @version OR version = -1) LIMIT 50";
             Add(cmd,"@zoneid",zoneId);Add(cmd,"@version",_zoneVersion);
             await using var r=await cmd.ExecuteReaderAsync(cancellationToken);
             while(await r.ReadAsync(cancellationToken))
